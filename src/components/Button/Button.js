de3e7ed9d2139674641e12055ce2008162b6eca7
erkhes01 <106813277+erkheseee@users.noreply.github.com>
2023-10-23 12:59:05 +0800

    early version of the login screen

--- src/components/Button/Button.js
+++ src/components/Button/Button.js
@@ -1,22 +1,60 @@
-import { View, Text, StyleSheet } from 'react-native';
+import { View, Text, StyleSheet, Pressable } from 'react-native';
 
-const Button = ({onPress, text}) => {
+const Button = ({onPress, text, type = "PRIMARY", bgColor, fgColor}) => {
 return (
-    <View onPress={onPress} style={styles.button}>
-        <Text>{text}</Text>
-    </View>
+    <Pressable 
+        onPress={onPress} 
+        style={[
+            styles.button, 
+            styles[`${type}_button`],
+            bgColor ? {backgroundColor: bgColor} : {},
+        ]}>
+        <Text style={[
+            styles.text, 
+            styles[`${type}_text`],
+            fgColor ? {color: fgColor} : {},
+        ]}>{text}</Text>
+    </Pressable>
 );
 };
 
 const styles = StyleSheet.create({
     button: {
-        backgroundColor: "lightblue",
         width: "50%",
         padding: 10,
         marginVertical: 10,
         alignItems: 'center',
         borderRadius: 30,
     },
+
+    PRIMARY_button: {
+        backgroundColor: "lightblue",
+    },
+
+    SECONDARY_button: {
+
+    },
+
+    TERTIARY_button: {
+
+    },
+
+    text: {
+        fontWeight: 'bold',
+    },
+
+    PRIMARY_text: {
+        color: 'white',
+    },
+    
+    SECONDARY_text: {
+
+    },
+    
+    TERTIARY_text: {
+        color: 'grey',
+    },
+    
 })
 
 export default Button;
--- src/screens/SignIn.js
+++ src/screens/SignIn.js
@@ -1,24 +1,81 @@
 import React, {useState} from "react";
-import {View, Text, Image, StyleSheet} from "react-native";
+import {View, Text, Image, StyleSheet, ScrollView} from "react-native";
 import Logo from '../../assets/images/logo.png';
-import InputField from '../components/Input/Input';
-import Button from '../components/Button/Button';
+import MyInputField from '../components/Input/Input';
+import MyButton from '../components/Button/Button';
 
 const SignIn = () => {
     const [userID, setUserID] = useState('');
     const [password, setPassword] = useState('');
+    const handleClickSignIn = () => {
+        console.warn("Sign in");
+    };
+    const handleClickForgotPassword = () => {
+        console.warn("Forgot password");
+    }
+
+    const handleClickSignInFacebook = () => {
+        console.warn("Sign in with Facebook");
+    }
+
+    const handleClickSignInGoogle = () => {
+        console.warn("Sign in with Google");
+    }
+
+    const handleClickSignUp = () => {
+        console.warn("Sign up");
+    }
 
     return(
-        <View>
-            <View style={styles.login}>
-                <Image source={Logo} style={styles.logo} resizeMode="contain"/>
-                <Text>Sign-In Here</Text>
-                <InputField placeholder="User ID" value={userID} setValue={setUserID}/>
-                <InputField placeholder="Password" value={password} setValue={setPassword} secureTextEntry={true}/>
-                <Button></Button>
+        <ScrollView showsVerticalScrollIndicator={false}>
+            <View>
+                <View style={styles.login}>
+                    <Image 
+                        source={Logo} 
+                        style={styles.logo} 
+                        resizeMode="contain"/>
+
+                    <Text>Sign-In Here</Text>
+
+                    <MyInputField 
+                        placeholder="User ID" 
+                        value={userID} 
+                        setValue={setUserID}/>
+
+                    <MyInputField 
+                        placeholder="Password" 
+                        value={password} 
+                        setValue={setPassword} 
+                        secureTextEntry={true}/>
+
+                    <MyButton 
+                        onPress={handleClickSignIn} 
+                        text="Нэвтрэх"/>
+
+                    <MyButton 
+                        onPress={handleClickForgotPassword} 
+                        text="Нууц үгээ мартсан уу?"
+                        type="TERTIARY"/>
+
+                    <MyButton 
+                        onPress={handleClickSignInFacebook} 
+                        text="Facebook-ээр нэвтрэх"
+                        bgColor="E7EAF4"
+                        fgColor="4765A9"/>
+
+                    <MyButton 
+                        onPress={handleClickSignInGoogle} 
+                        text="Google-ээр нэвтрэх"
+                        bgColor="FAE9EA"
+                        fgColor="DD4D44"/>
+
+                    <MyButton 
+                        onPress={handleClickSignUp} 
+                        text="Бүртгэл байхгүй юу?"
+                        type="TERTIARY"/>
+                </View>
             </View>
-            
-        </View>
+        </ScrollView>
     );
 };
 
