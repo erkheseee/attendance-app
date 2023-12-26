77c1e04714537e2e9291b76b4a8a764d12331f6f
erkhes01 <106813277+erkheseee@users.noreply.github.com>
2023-12-26 17:43:11 +0800

    new lec lab data

--- src/screens/ClassScreens/Grades/AttendanceTable.jsx
+++ src/screens/ClassScreens/Grades/AttendanceTable.jsx
@@ -1,64 +1,116 @@
-import { View, ScrollView, Text, StyleSheet, TouchableOpacity } from 'react-native'
-import React, {useState} from 'react'
+import { View, ScrollView, useWindowDimensions, StyleSheet, ImageBackground, Text, TouchableOpacity } from 'react-native'
+import React, { useState } from 'react';
 import { Table, TableWrapper, Row, Rows, Col } from 'react-native-table-component';
-import AttendanceData from './AttendanceData';
+import { TabView, SceneMap, TabBar } from 'react-native-tab-view';
 
 const AttendanceTable = () => {
-
-    const [rata, setRata] = useState({});
-
-    const tableHead = ['Долоо хоног', 'Оноо', 'Огноо'];
+    const tableHead = ['Долоо хоног', 'Ирц', 'Огноо'];
     const tableData = [
-        ['1-р', '2/2', '2023-09-06'],
-        ['2-р', '1/2', '2023-09-13'],
-        ['3-р', '2/2', '2023-09-20'],
-        ['4-р', '2/2', '2023-09-27'],
-        ['5-р', '0/2', '2023-10-04'],
-        ['6-р', '0/2', '2023-10-11'],
-        ['7-р', '1/2', '2023-10-18'],
-        ['8-р', '0/2', '2023-10-25'],
-        ['9-р', '2/2', '2023-11-01'],
-        ['10-р', '2/2', '2023-11-08'],
-        ['11-р', '1/2', '2023-11-15'],
-        ['12-р', '2/2', '2023-11-22'],
-        ['13-р', '2/2', '2023-11-29'],
-        ['14-р', '1/2', '2023-12-06'],
+        ['Лекц 1', '+', '2023-09-06'],
+        ['Лекц 2', '+', '2023-09-13'],
+        ['Лекц 3', '+', '2023-09-20'],
+        ['Лекц 4', '+', '2023-09-27'],
+        ['Лекц 5', '-', '2023-10-04'],
+        ['Лекц 6', '-', '2023-10-11'],
+        ['Лекц 7', '+', '2023-10-18'],
+        ['Лекц 8', '-', '2023-10-25'],
+        ['Лекц 9', '+', '2023-11-01'],
+        ['Лекц 10', '+', '2023-11-08'],
+        ['Лекц 11', '-', '2023-11-15'],
+        ['Лекц 12', '-', '2023-11-22'],
+        ['Лекц 13', '+', '2023-11-29'],
+        ['Лекц 14', '-', '2023-12-06'],
     ];
-    const tableFoot = ['Нийт', '19/28', '']
-    const tableObject = AttendanceData();
+    const tableData2 = [
+        ['Лаб 1', '-', '2023-09-06'],
+        ['Лаб 2', '-', '2023-09-13'],
+        ['Лаб 3', '+', '2023-09-20'],
+        ['Лаб 4', '+', '2023-09-27'],
+        ['Лаб 5', '+', '2023-10-04'],
+        ['Лаб 6', '+', '2023-10-11'],
+        ['Лаб 7', '+', '2023-10-18'],
+        ['Лаб 8', '-', '2023-10-25'],
+        ['Лаб 9', '+', '2023-11-01'],
+        ['Лаб 10', '-', '2023-11-08'],
+        ['Лаб 11', '+', '2023-11-15'],
+        ['Лаб 12', '+', '2023-11-22'],
+        ['Лаб 13', '+', '2023-11-29'],
+        ['Лаб 14', '-', '2023-12-06'],
+    ];
+    const tableFoot = ['Нийт', '8', '14'];
+    const tableFoot2 = ['Нийт', '9', '14'];
 
-    const handlePress = (index) => {
-        setRata(current => ({...current, id: index}));
-    }
+    const FirstRoute = () => (
+        <ScrollView style={styles.tableContainer}>
+        <Table>
+            {/* <Row data={tableHead} flexArr={[1, 1, 1]} style={styles.head} textStyle={styles.text} /> */}
+            <View style={{backgroundColor: 'white', height: 1, marginBottom: 10,}}></View>
+            {
+                tableData.map(data => (
+                    <Row data={data} flexArr={[1, 1]} style={styles.row} textStyle={styles.text} />
+                ))
+            }
+            <View style={{backgroundColor: 'white', height: 1, marginTop: 10,}}></View>
+            <Row data={tableFoot} flexArr={[1, 1, 1]} style={styles.head} textStyle={styles.text} />
+        </Table>
+    </ScrollView>
+    );
 
-    const handleDropDown = (index) => {
-        const lec = [`Лекц`, `${tableObject[index]['lec']}`, ``];
-        const lab = [`Лаб`, `${tableObject[index]['lab']}`, ``];
-        return (
-            <>
-                <Row data={lec}  flexArr={[20, 1, 20]} style={styles.row} textStyle={styles.text} />
-                <Row data={lab}  flexArr={[20, 1, 20]} style={styles.row} textStyle={styles.text} />
-            </>
-        )
-    }
+    const SecondRoute = () => (
+        <ScrollView style={styles.tableContainer}>
+        <Table>
+            {/* <Row data={tableHead} flexArr={[1, 1, 1]} style={styles.head} textStyle={styles.text} /> */}
+            <View style={{backgroundColor: 'white', height: 1, marginBottom: 10,}}></View>
+            {
+                tableData2.map(data => (
+                    <Row data={data} flexArr={[1, 1]} style={styles.row} textStyle={styles.text} />
+                ))
+            }
+            <View style={{backgroundColor: 'white', height: 1, marginTop: 10,}}></View>
+            <Row data={tableFoot2} flexArr={[1, 1, 1]} style={styles.head} textStyle={styles.text} />
+        </Table>
+    </ScrollView>
+    );
+
+    const renderScene = SceneMap({
+        first: FirstRoute,
+        second: SecondRoute,
+    });
+
+    const layout = useWindowDimensions();
+
+    const [index, setIndex] = useState(0);
+    const [routes] = useState([
+        { key: 'first', title: 'Лекц' },
+        { key: 'second', title: 'Лаб' },
+    ]);
+
 
     return (
-        <ScrollView style={styles.tableContainer}>
-            <Table>
-                <Row data={tableHead} flexArr={[1, 1, 1]} style={styles.head} textStyle={styles.text} />
-                <View style={{backgroundColor: 'white', height: 1, marginBottom: 10,}}></View>
-                        {
-                            tableData.map((data, index) => (
-                                <TouchableOpacity onPress={() => handlePress(index)}>
-                                    <Row data={data} flexArr={[1, 1, 1]} style={styles.row} textStyle={styles.text} />
-                                    {rata['id'] == index && handleDropDown(index)}
-                                </TouchableOpacity>
-                            ))
-                        }
-                <View style={{backgroundColor: 'white', height: 1, marginTop: 10,}}></View>
-                <Row data={tableFoot} flexArr={[1, 1, 1]} style={styles.head} textStyle={styles.text} />
-            </Table>
-        </ScrollView>
+        <TabView
+            navigationState={{ index, routes }}
+            renderScene={renderScene}
+            onIndexChange={setIndex}
+            initialLayout={{ width: layout.width }}
+            renderTabBar={props =>
+                <TabBar
+                    {...props}
+                    indicatorStyle={{ backgroundColor: '#D9BDBC' }}
+                    style={{ backgroundColor: '#9b4e4d', borderTopWidth: 1}}
+                    labelStyle={{
+                        fontSize: 12,
+                        fontWeight: "bold",
+                        color: 'white',
+                        alignSelf: 'center',
+                        shadowColor: 'black',
+                        shadowOpacity: 1,
+                        shadowRadius: 0,
+                        shadowOffset: { height: 2, width: 2 },
+                    }}
+                    tabStyle={{ alignContent: 'space-between' }}
+                />
+            }
+        />
     )
 }
 
